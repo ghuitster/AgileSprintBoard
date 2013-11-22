bebc67ec074a249e4ef1f03d22446d7b16bac90d
Mark Siebert <mark.jiali.siebert@gmail.com>
2013-11-22 14:59:20 -0700

    updated database schema, added sprints

--- sql/1.sql
+++ sql/1.sql
@@ -21,7 +21,17 @@ CREATE TABLE `boards` (
 CREATE TABLE `users_boards` (
 	`user_id` BINARY(16) NOT NULL,
 	`board_id` BINARY(16) NOT NULL,
+	`privileges` TINYINT NOT NULL DEFAULT 0, #0 will be admin
 	PRIMARY KEY (`user_id`, `board_id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE `sprints` (
+	`hiddenid` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`id` BINARY(16) NOT NULL,
+	`start` DATETIME NOT NULL,
+	`end` DATETIME NOT NULL,
+	`board_id` BINARY(16) NOT NULL,
+	PRIMARY KEY (`hiddenid`)
 ) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_general_ci;
 
 CREATE TABLE `stories` (
@@ -30,6 +40,7 @@ CREATE TABLE `stories` (
 	`name` VARCHAR(100) NOT NULL,
 	`description` VARCHAR(300),
 	`estimate` DECIMAL(3, 1) NOT NULL,
+	`sprint_id` BINARY(16), #if null, then it belongs to the backlog
 	PRIMARY KEY (`hiddenid`),
 	KEY `name` (`name`)
 ) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -48,15 +59,10 @@ CREATE TABLE `tasks` (
 	`estimate` DECIMAL(3, 1) NOT NULL,
 	`completed` BOOLEAN NOT NULL DEFAULT FALSE
 	`completion_date` DATETIME,
+	`story_id` BINARY(16) NOT NULL,
 	PRIMARY KEY (`hiddenid`),
 	KEY `name` (`name`),
 	KEY `completed` (`completed`)
-) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_general_ci;
-
-CREATE TABLE `tasks_stories` (
-	`task_id` BINARY(16) NOT NULL,
-	`story_id` BINARY(16) NOT NULL,
-	PRIMARY KEY (`task_id`, `story_id`)
 ) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_general_ci;
 
 CREATE TABLE `users_tasks` (
