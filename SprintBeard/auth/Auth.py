4abfc84da208a97d31e82f6f0dc796b2b59dcdb3
Mark Siebert <mark.jiali.siebert@gmail.com>
2013-11-26 20:06:18 -0700

    added authorized function decorator (authenticated user owns resource), also added gitignore rule to ignore files ending in ~ (I'm looking at you, Nate)

--- SprintBeard/auth/Auth.py
+++ SprintBeard/auth/Auth.py
@@ -1,4 +1,4 @@
-from flask import redirect, session, url_for
+from flask import redirect, render_template, session, url_for
 
 '''
 This module provides decorator functions that enforce authentication. To use these functions, import the module, and then use the decorator above the method which handles the request. The authentication decorator MUST come after any other decorators for this to work. Ex:
@@ -25,3 +25,21 @@ def authenticated(handler):
 
 	return do_auth
 
+def authorized(handler):
+	'''
+	A function decorator which requires that a user is authenticated, and that the user id passed in matches the authenticated user's id.
+		arg: handler - a function which handles a request. Must take 'user_id' as the first argument to the function.
+
+		return: the handler augmented such that it requires authentication
+	'''
+	def do_authorized(*args, **kwargs):
+		if 'user' not in session:
+			return redirect(url_for('login'))
+			
+		#make sure the userids match
+		if session['user'].id == kwargs['user_id']:
+			return handler(*args, **kwargs)
+		else:
+			return render_template('auth/unauthorized.html')
+
+	return do_authorized
--- SprintBeard/controllers/UserController.py
+++ SprintBeard/controllers/UserController.py
@@ -1,3 +1,4 @@
+from auth import Auth
 from flask import Blueprint, render_template
 from models import Users
 from uuid import UUID
@@ -5,6 +6,7 @@ from uuid import UUID
 users = Blueprint('users', __name__)
 
 @users.route('/users/<user_id>', methods=['GET'])
+@Auth.authorized
 def view(user_id):
 	user = Users.get(UUID(user_id))
 	return render_template('boards/dashboard.html', user=user)
