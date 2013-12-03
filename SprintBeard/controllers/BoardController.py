faf8009e3b9d53068dca759892e06a4f8ac76f6e
The Beard <nwatterson@gmail.com>
2013-12-02 22:18:16 -0700

    Update the Boards.py to Include Comments

--- SprintBeard/controllers/BoardController.py
+++ SprintBeard/controllers/BoardController.py
@@ -4,12 +4,12 @@ from uuid import UUID
 
 boards = Blueprint('boards', __name__)
 
-#@boards.route('/user/<user_id>/boards', methods=['GET'])
-#def view(user_id):
-#	user = Users.get(UUID(user_id))	# Get User from User.py
-#	boardList = Boards.getUserBoards(user_id)
-#	return render_template('boards/dashboard.html', user=user, boardList=boardList)
+@boards.route('/user/<user_id>/boards', methods=['GET'])
+def view(user_id):
+	user = Users.get(UUID(user_id))	# Get User from User.py
+	boardList = Boards.getUserBoards(user_id)
+	return render_template('boards/dashboard.html', user=user, boardList=boardList)
 
-@boards.route('/user/boards', methods=['GET'])
-def view():
-	return render_template('boards/dashboard.html', user='Beard')
+#@boards.route('/user/boards', methods=['GET'])
+#def view():
+#	return render_template('boards/dashboard.html', user='Beard')
--- SprintBeard/models/Boards.py
+++ SprintBeard/models/Boards.py
@@ -4,12 +4,22 @@ import flask
 
 db = MySQLdb.connect(host='localhost', user='dev', passwd='dev', db='agile')
 
-class Board:						# Class holds NAME and ID for a given Board
+class Board:
+	'''
+	A class that holds data about a Board.
+		field: name - the Name of the Board (string)
+		field: id - the id of the Board (string)
+	'''
 	def __init__(self, name, id = uuid.uuid4() ):
 		self.name = name
 		self.id = id
 
-def createBoard(userID):					# Adds 'default' board to DB. Returns the Board
+def createBoard(userID):
+	'''
+	Create a Board for a certain userID
+		arg: userID - the id of the user creating the board
+		return: a Default Board
+	'''
 	board = Board("Default Board");
 	cursor = db.cursor()
 	cursor.execute('''INSERT INTO boards (id, name) VALUES (%s, '%s')''', (board.id.bytes, board.name) )
@@ -20,7 +30,12 @@ def createBoard(userID):					# Adds 'default' board to DB. Returns the Board
 	except:
 		db.rollback()
 
-def changeName(boardID, newName):	# Changes the Name of a Board in the DB
+def changeName(boardID, newName):
+	'''
+	Change the Name attribute of a Board for a certain boardID
+		arg: boardID - the id of the Board to be modified
+		arg: newName - the new Name of the Board
+	'''
 	cursor = db.cursor()
 	cursor.execute('''UPDATE boards SET name=%s WHERE id=%s''', (newName, boardID))
 	try:
@@ -28,7 +43,12 @@ def changeName(boardID, newName):	# Changes the Name of a Board in the DB
 	except:
 		db.rollback()
 
-def getUserBoards(userID):			# get all the boards for the user
+def getUserBoards(userID):
+	'''
+	Reurn a list of all Boards associated with a userID
+		arg: userID - the id of the user
+		return: a List of Boards
+	'''
 	cursor = db.cursor()
 	userBoards = cursor.execute('''select user_id, board_id, privileges, name from users_boards inner join boards on boards.id = users_boards.board_id where users_boards.user_id="%s"''', (userID))
 	
