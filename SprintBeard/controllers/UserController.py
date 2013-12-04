5d02a818606aa19921b21de73e97eb1bd64c32ae
The Beard <nwatterson@gmail.com>
2013-12-03 20:23:28 -0700

    update Layout

--- SprintBeard/controllers/UserController.py
+++ SprintBeard/controllers/UserController.py
@@ -9,3 +9,9 @@ users = Blueprint('users', __name__)
 def view(user_id):
 	user = Users.get(user_id)
 	return render_template('users/view.html', user=user)
+
+@users.route('/users/<user_id>/settings', methods=['GET'])
+@Auth.authorized(Auth.USER_AUTHORIZATION)
+def settings(user_id):
+	user = Users.get(user_id)
+	return render_template('users/settings.html', user=user)
