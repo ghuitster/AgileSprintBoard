0855b384e2534afa16e00e3264740211e79cd334
kingjamesiv <daniel.ricks2@gmail.com>
2013-12-16 14:41:23 -0700

    Added an edit method for Stories

--- SprintBeard/controllers/StoryController.py
+++ SprintBeard/controllers/StoryController.py
@@ -37,6 +37,39 @@ def create(board_id):
 		return '{"status": "success"}'
 	else:
 		return json.dumps(result)
+		
+@stories.route('/stories/<story_id>/edit', methods=['POST'])
+@Auth.authorized(Auth.STORY_AUTHORIZATION, Auth.MALFORMED_UUID_JSON)
+def edit(story_id):
+	'''
+	Edits a story in the given board
+		arg: board_id - the id of the board on which the story is edited
+		POST arg: name - the name of the story
+		POST arg: description - a description of the story
+		POST arg: estimate - an estimate of story points
+		POST arg: sprint_id - the id of the sprint to edit, 'backlog' for backlog,
+			or 'current' for the current sprint
+	'''
+	name = request.form['name']
+	description = request.form['description']
+	estimate = float(request.form['estimate'])
+	sprint_id = (request.form['sprint_id']).lower()
+	board_id = request.form['board_id']
+
+	#get the correct sprint id if one of the options is specified
+	if sprint_id == 'backlog':
+		sprint_id = None
+	elif sprint_id == 'current':
+		sprint = Sprints.get_current_sprint(board_id)
+		if sprint is not None:
+			sprint_id = sprint.id
+
+	result = Stories.create(story_id, name, description, estimate, board_id, sprint_id)
+
+	if type(result) != dict:
+		return '{"status": "success"}'
+	else:
+		return json.dumps(result)
 
 @stories.route('/stories/<story_id>', methods=['DELETE'])
 @Auth.authorized(Auth.STORY_AUTHORIZATION, Auth.MALFORMED_UUID_JSON)
